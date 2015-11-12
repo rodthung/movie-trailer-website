2827454537358c0bbae0e4bbef250dc88b4131ba
Tank <r.sudrung@gmail.com>
2015-11-12 00:29:50 -0800

    add released_year instance variable

--- media.py
+++ media.py
@@ -4,8 +4,9 @@ import webbrowser
 class Movie():
 
 	#contructor for creating Movie instance
-	def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+	def __init__(self, movie_title,movie_released_year, movie_storyline, poster_image, trailer_youtube):
 		self.title = movie_title
+		self.released_year = movie_released_year
 		self.story_line = movie_storyline
 		self.poster_image_url = poster_image
 		self.trailer_youtube_url = trailer_youtube
