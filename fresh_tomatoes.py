129973278005d5c273eafc55b638e429d8f77898
Tank <r.sudrung@gmail.com>
2015-11-12 00:38:17 -0800

    modified create_movie_tiles_content and html to show movie story and released year

--- fresh_tomatoes.py
+++ fresh_tomatoes.py
@@ -121,7 +121,7 @@ main_page_content = '''
 movie_tile_content = '''
 <div class="col-md-6 col-lg-4 movie-tile text-center" data-trailer-youtube-id="{trailer_youtube_id}" data-toggle="modal" data-target="#trailer">
     <img src="{poster_image_url}" width="220" height="342">
-    <h2>{movie_title}</h2><h4>({movie_release_year})</h4>
+    <h2>{movie_title}</h2><h4>({movie_released_year})</h4>
     <p>{movie_story}</p>
 </div>
 '''
@@ -138,7 +138,7 @@ def create_movie_tiles_content(movies):
         # Append the tile for the movie with its content filled in
         content += movie_tile_content.format(
             movie_title=movie.title,
-            movie_release_year=movie.released_year,
+            movie_released_year=movie.released_year,
             movie_story=movie.story_line,
             poster_image_url=movie.poster_image_url,
             trailer_youtube_id=trailer_youtube_id
